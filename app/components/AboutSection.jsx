c20f2547ce82c93bf27d2a183cfc2efa5bb3f24a
Ahsaask <109789003+Ahsaask@users.noreply.github.com>
2024-09-28 02:08:49 +0000

    removed list item about section

--- app/components/AboutSection.jsx
+++ app/components/AboutSection.jsx
@@ -23,7 +23,6 @@ const TAB_DATA = [
     id: "education",
     content: (
       <ul className="list-disc pl-2">
-        <li>Books</li>
         <li>University of Calgary, Alberta</li>
       </ul>
     ),
